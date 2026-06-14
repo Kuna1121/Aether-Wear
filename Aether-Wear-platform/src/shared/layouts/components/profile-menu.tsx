f1f829cf013c69c043284922a24db714edf7229e
Kuna1121 <161556581+Kuna1121@users.noreply.github.com>
2026-06-14 14:45:50 +0530

    changes

--- Aether-Wear-platform/src/shared/layouts/components/profile-menu.tsx
+++ Aether-Wear-platform/src/shared/layouts/components/profile-menu.tsx
@@ -1,37 +1,53 @@
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import type { Users } from "../../models";
 import { logout } from "../services";
+import "./profile-menu.css";
 
 export default function ProfileMenu({ user }: { user: Users }) {
   const navigate = useNavigate();
+
   const handleLogout = () => {
-    const logedOut = logout();
-    if (logedOut) {
+    const loggedOut = logout();
+
+    if (loggedOut) {
       navigate("/login");
     }
   };
 
   return (
-    <>
-      <div className="nav-user-profile">
-        <div className="user-avatar-circle">{user?.name.charAt(0)}</div>
-        <div className="user-dropdown-menu">
-          <div className="user-dropdown-header">
-            <span className="user-profile-name">{user?.name}</span>
-            <span className="user-profile-email">{user?.email}</span>
-          </div>
-          <div className="dropdown-divider"></div>
-          <Link to="/profile" className="dropdown-item">
-            My Profile
-          </Link>
-          <Link to="/order-history" className="dropdown-item">
-            Orders History
-          </Link>
-          <div className="dropdown-item logout-item" onClick={handleLogout}>
-            Logout
-          </div>
+    <div className="nav-user-profile">
+      <div className="user-avatar-circle">{user?.name?.charAt(0)}</div>
+
+      <div className="user-dropdown-menu">
+        <div className="user-dropdown-header">
+          <span className="user-profile-name">{user?.name}</span>
+          <span className="user-profile-email">{user?.email}</span>
+        </div>
+
+        <div className="dropdown-divider"></div>
+
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            `dropdown-item ${isActive ? "active" : ""}`
+          }
+        >
+          My Profile
+        </NavLink>
+
+        <NavLink
+          to="/order-history"
+          className={({ isActive }) =>
+            `dropdown-item ${isActive ? "active" : ""}`
+          }
+        >
+          Orders History
+        </NavLink>
+
+        <div className="dropdown-item logout-item" onClick={handleLogout}>
+          Logout
         </div>
       </div>
-    </>
+    </div>
   );
 }
